c0f63731618d3b88d4eaf329627870e4bda63465
Pablo Palencia <ppalenciar1@miumg.edu.gt>
2025-04-11 09:35:34 -0600

    sistema terminado

--- ProyectoFinalExamen/src/main/java/Modelo/seguridad/EmpresaDAO.java
+++ ProyectoFinalExamen/src/main/java/Modelo/seguridad/EmpresaDAO.java
@@ -6,6 +6,7 @@
 package Modelo.seguridad;
 
 import Controlador.seguridad.Empresa; 
+import Modelo.Conexion;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
